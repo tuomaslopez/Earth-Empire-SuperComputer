cc6432cf479db383c38df20e4e87b7bc4e682b10
tuomaslopez <112478934+tuomaslopez@users.noreply.github.com>
2023-02-20 21:02:51 +0200

    Delete militaryExpencesmoney.h

--- militaryExpencesmoney.h
+++
@@ -1,9 +0,0 @@
-#ifndef MILITARYEXPENCESMONEY_H
-#define MILITARYEXPENCESMONEY_H
-
-#endif // MILITARYEXPENCESMONEY_H
-
-
-
-int militaryExpencesmoney(int troops, int jets, int turrets , int tanks , int spies, double networth);
-
