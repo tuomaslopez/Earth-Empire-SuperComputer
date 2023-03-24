9fef9f09970d44fb441c0d55d046de7b3a8baac3
tuomaslopez <112478934+tuomaslopez@users.noreply.github.com>
2023-03-24 11:21:16 +0200

    Delete doubleToString.cpp

--- doubleToString.cpp
+++
@@ -1,9 +0,0 @@
-#include "doubleToString.h"
-#include <iostream>
-#include <sstream>
-std::string doubleToString(double val)
-{
-    std::ostringstream out;
-    out << val;
-    return out.str();
-}
