cd1039bf49ba62044cff2b16daff9cb45a25520e
tuomaslopez <112478934+tuomaslopez@users.noreply.github.com>
2023-03-24 11:21:09 +0200

    Delete Maxpop.h

--- Maxpop.h
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <streambuf>
-#include <iostream>
-
-
-
-int Maxpop(float taxDouble, int landINT, std::string land, double BuildingResidences  , double businessTech);
